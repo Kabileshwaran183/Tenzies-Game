e78e6761b1b149959b3e19bd677b4f93c9f85613
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-24 19:08:39 +0530

    Cafetti - decorating the winning moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Die from "./components/Die"
 import { useState } from "react"
 import { nanoid } from "nanoid"
-
+import Confetti from "react-confetti"
 export default function App() {
     const [dice, setDice] = useState(()=>generateAllNewDice())
 
@@ -18,13 +18,17 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice(oldDice => oldDice.map(die =>
-            die.isHeld ?
-                die :
-                { ...die, value: Math.ceil(Math.random() * 6) }
-        ))
-
+        if (!gameWon) {
+            setDice(oldDice => oldDice.map(die =>
+                die.isHeld ?
+                    die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            ))
+        } else {
+            setDice(generateAllNewDice())
+        }
     }
+
 
     function hold(id) {
         setDice(oldDice => oldDice.map(die =>
@@ -46,6 +50,9 @@ export default function App() {
 
     return (
         <main>
+            {gameWon && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
